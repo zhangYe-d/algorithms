b0741e0713377b7f6a088434a405d9efaaf49ffc
zhangye <13244619815@163.com>
2022-09-20 18:40:57 +0800

    feat: 完成归并排序逻辑

--- src/mergeSort.js
+++ src/mergeSort.js
@@ -2,12 +2,40 @@ const mergeSort = sourceArr => {
 	if (!Array.isArray(sourceArr)) return
 	if (sourceArr.length <= 1) return sourceArr
 
-	const mid = sourceArr >> 1
+	const mid = sourceArr.length >> 1
 
-	return merge(
-		sourceArr.slice(0, mid + 1),
-		sourceArr.slice(mid + 1, sourceArr.length)
-	)
+	const sortedLeft = mergeSort(sourceArr.slice(0, mid))
+	const sortedRight = mergeSort(sourceArr.slice(mid))
+
+	return merge(sortedLeft, sortedRight)
 }
 
-const merge = (left, right) => {}
+const merge = (left, right) => {
+	const mergedArray = []
+	let i = 0
+	let j = 0
+
+	while (i < left.length && j < right.length) {
+		if (left[i] < right[j]) {
+			mergedArray.push(left[i])
+			i++
+		} else {
+			mergedArray.push(right[j])
+			j++
+		}
+	}
+
+	while (i < left.length) {
+		mergedArray.push(left[i])
+		i++
+	}
+
+	while (j < right.length) {
+		mergedArray.push(right[j])
+		j++
+	}
+
+	return mergedArray
+}
+
+// console.log(mergeSort([6, 4, 9, 1, 0]))
