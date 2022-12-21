39507e1198868cbf415fd91e5973bb3e20956350
zhangye <13244619815@163.com>
2022-12-21 19:52:24 +0800

    leetcode: 实现求解二进制数中1的个数算法

---
+++ src/algorithms/leetcode/low/hammingWeight.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+	let count = 0
+	while (n) {
+		n &= n - 1
+		count++
+	}
+
+	return count
+}
