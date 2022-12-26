8c702920e190232ae4117dca959861c2f33ffb07
zhangye <13244619815@163.com>
2022-12-26 20:54:02 +0800

    leetcode: 实现合并两个链表算法

--- src/algorithms/leetcode/low/mergeTwoLists.js
+++ src/algorithms/leetcode/low/mergeTwoLists.js
@@ -1,46 +1,32 @@
-/**合并两个升序链表
+/**
  * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
  * }
  */
 /**
- * @param {ListNode} list1
- * @param {ListNode} list2
+ * @param {ListNode} l1
+ * @param {ListNode} l2
  * @return {ListNode}
  */
-var mergeTwoLists = function (list1, list2) {
-	if (list1 === null) {
-		return list2
-	}
+var mergeTwoLists = function (l1, l2) {
+	const preNode = new ListNode(null)
+	let curNode = preNode
 
-	if (list2 === null) {
-		return list1
-	}
-
-	let prehead = new ListNode(null)
-	let curNode = prehead
-
-	while (list1 !== null && list2 !== null) {
-		if (list1.val < list2.val) {
-			curNode.next = list1
-			list1 = list1.next
+	while (l1 || l2) {
+		const val1 = l1?.val ?? Infinity
+		const val2 = l2?.val ?? Infinity
+		if (val1 < val2) {
+			curNode.next = l1
+			l1 = l1.next
 		} else {
-			curNode.next = list2
-			list2 = list2.next
+			curNode.next = l2
+			l2 = l2.next
 		}
 
 		curNode = curNode.next
 	}
 
-	if (list1 !== null) {
-		curNode.next = list1
-	}
-
-	if (list2 !== null) {
-		curNode.next = list2
-	}
-
-	return prehead.next
+	return preNode.next
 }
