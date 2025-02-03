9e04432c158188b355f6630400da6e104a858c13
zhangYe <daizhichun621@gmail.com>
2025-02-03 23:00:04 +0800

    leetcode threeSum refactor

--- src/algorithms/leetcode/middle/threeSum.ts
+++ src/algorithms/leetcode/middle/threeSum.ts
@@ -1,29 +1,33 @@
 function threeSum(nums: number[]): number[][] {
   nums.sort((num1, num2) => num1 - num2);
   const res: [number, number, number][] = [];
-  let lastFirst = Infinity,
-    lastSecond = Infinity;
+  let lastFirst = Infinity;
   for (let i = 0; i < nums.length; i++) {
     const first = nums[i];
     if (first === lastFirst) continue;
     lastFirst = first;
-    for (let j = i + 1; j < nums.length; j++) {
-      const second = nums[j];
-      if (second === lastSecond) continue;
-      lastSecond = second;
-      const third = -(first + second);
-      let start = j + 1,
-        end = nums.length - 1;
-      while (start <= end) {
-        const mid = (start + end) >> 1;
-        if (nums[mid] === third) {
-          res.push([first, second, third]);
-          break;
-        } else if (nums[mid] > third) {
-          end = mid - 1;
-        } else {
-          start = mid + 1;
+    let j = i + 1,
+      k = nums.length - 1;
+    let second = nums[j],
+      third = nums[k];
+    while (j < k) {
+      const sum = first + second + third;
+      if (sum === 0) {
+        res.push([first, second, third]);
+        while (nums[j] === second) {
+          j++;
         }
+        second = nums[j];
+      } else if (sum > 0) {
+        while (nums[k] === third) {
+          k--;
+        }
+        third = nums[k];
+      } else {
+        while (nums[j] === second) {
+          j++;
+        }
+        second = nums[j];
       }
     }
   }
